f52b94895e29f042c0dcf9f6e7334894078de188
NickoJo <45911495+NickoJo@users.noreply.github.com>
2019-11-18 22:02:06 +0300

    changed only one string

--- main.py
+++ main.py
@@ -1,3 +1,48 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+
+
+HEXADECIMAL_NUMBERS = '0123456789ABCDEF'
+
+
+def to_decimal(num, base):  # функция для перевода числа в десятичную СС
+    num = [HEXADECIMAL_NUMBERS.index(digit) for digit in str(num)]
+    return sum([num[-i - 1] * base ** i for i in range(len(num))])
+
+
+def from_decimal(num, base):  # функция для перевода числа из десятичной СС
+    res = []
+    while num != 0:
+        res.append(num % base)
+        num //= base
+
+    res = list(map(lambda d: HEXADECIMAL_NUMBERS[d], res))[::-1]
+    return ''.join(res)
+
+
+class InterfaceWidget(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.start_button = None
+        self.task_type, self.range, self.filename = None, [None, None], None
+        self.init_ui()
+        self.show()
+
+    def init_ui(self):
+        self.setFixedSize(800, 600)
+        self.setWindowTitle('Примеры по информатике')
+
+        self.start_button = QPushButton('Создать файл с примерами')
+        self.start_button.clicked.connect(self.start)
+
+    def start(self):
+        generator = NumeralSystems(self.task_type, self.range[0],
+                                   self.range[1], self.filename)
+        generator.start()
+
+
 class NumeralSystems:
 
     def __init__(self, task, start, end, file):
@@ -6,11 +51,14 @@ class NumeralSystems:
         self.end = end
         self.file = file
 
-    def task_type(self):
-        pass  # обработка self.task
+    def variables(self):
+        pass
 
-    def values(self):
-        pass  # обработка self.start и self.end (random)
 
-    def file_name(self):
-        pass  # обработка self.file (os?)
+if __name__ == '__main__':
+    try:
+        app = QApplication(sys.argv)
+        gui = InterfaceWidget()
+        sys.exit(app.exec())
+    except Exception as e:
+        print('Ошибка:', e)
