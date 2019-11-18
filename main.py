528b2a082dda4abd8f29687395f8ef0740fe53f3
NikolayNyunin <n.nyunin@mail.ru>
2019-11-18 21:29:27 +0300

    Added functions to_decimal and from_decimal

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+HEXADECIMAL_NUMBERS = '0123456789ABCDEF'
+
+
+def to_decimal(num, base):  # функция для перевода числа в десятичную СС
+    num = [HEXADECIMAL_NUMBERS.index(digit) for digit in str(num)]
+    return sum([num[-i - 1] * base ** i for i in range(len(num))])
+
+
+def from_decimal(num, base):  # функция для перевода числа из десятичной СС
+    res = []
+    while num != 0:
+        res.append(num % base)
+        num //= base
+
+    res = list(map(lambda d: HEXADECIMAL_NUMBERS[d], res))[::-1]
+    return ''.join(res)
+
+
 class NumeralSystems:
 
     def __init__(self, task, start, end, file):
@@ -14,3 +32,11 @@ class NumeralSystems:
 
     def file_name(self):
         pass  # обработка self.file (os?)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
