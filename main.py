0051c02a8afa8849466485c1498021579568abd4
NickoJo <45911495+NickoJo@users.noreply.github.com>
2019-11-18 20:54:06 +0300

    added some methods

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 class NumeralSystems:
+
+    def __init__(self, task, start, end, file):
+        self.task = task
+        self.start = start
+        self.end = end
+        self.file = file
+
+    def task_type(self):
+        pass  # обработка self.task
+
+    def values(self):
+        pass  # обработка self.start и self.end (random)
+
+    def file_name(self):
+        pass  # обработка self.file (os?)
